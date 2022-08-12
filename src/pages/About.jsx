b2fb5d576899653c70704e7ddfb82650067c52de
trav15 <travistokuyama@gmail.com>
2022-08-12 13:53:14 +0100

    Add Home, About, and NotFound pages and routes

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+function About() {
+  return (
+    <div>
+        <h1 className="text-6xl mb-4">Github Finder</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit iure similique aliquid est, earum quam aspernatur nisi iste saepe quod quae corporis soluta consequatur eius iusto ipsam explicabo ratione id.</p>
+        <p className='text-lg text-gray-400'>
+            Version <span className=''>1.0.0</span>
+        </p>
+    </div>
+  )
+}
+
+export default About
