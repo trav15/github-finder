f32d9dc3249a553b828564bd86b3708a896cd06f
trav15 <travistokuyama@gmail.com>
2022-08-12 15:25:42 +0100

    Add Reducer hook for state management

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,25 +1,32 @@
-import { createContext, useState } from 'react'
+import { createContext, useReducer } from 'react';
+import githubReducer from './GithubReducer';
 
 const GithubContext = createContext();
 
 const GITHUB_URL = "https://api.github.com"
 
 export const GithubProvider = ({children}) => {
-    const [users, setUsers] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const initialState = {
+        users: [],
+        loading: true
+    }
+
+    const [state, dispatch] = useReducer(githubReducer, initialState)
 
     const fetchUsers = async () => {
         const response = await fetch(`${GITHUB_URL}/users`)
         
         const data = await response.json();
         
-        setUsers(data);
-        setLoading(false);
+    dispatch({
+        type: 'GET_USERS',
+        payload: data,
+    })
     }
 
     return <GithubContext.Provider value={{
-        users, 
-        loading,
+        users: state.users, 
+        loading: state.loading,
         fetchUsers,
     }}>
         {children}
