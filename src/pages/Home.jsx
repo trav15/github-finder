99c2a53945cb623beb6f423da87201f5951dc0d2
trav15 <travistokuyama@gmail.com>
2022-08-12 14:56:54 +0100

    Add UserItem component to display Users as card with avatar and info

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import UserResults from '../components/users/UserResults';
 function Home() {
   return (
     <div>
-        <h1 className="text-6xl">Welcome</h1>
+        <h1 className="text-6xl pb-6">Welcome To The Finder</h1>
         <UserResults />
     </div>
   )
