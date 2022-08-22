24d5dfa8151a6c74b85f7223d5e714db846aa147
trav15 <travistokuyama@gmail.com>
2022-08-22 11:30:42 +0100

    Update info in About section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,9 +2,9 @@ function About() {
   return (
     <div>
         <h1 className="text-6xl mb-4">Github Finder</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit iure similique aliquid est, earum quam aspernatur nisi iste saepe quod quae corporis soluta consequatur eius iusto ipsam explicabo ratione id.</p>
+        <p>This is an app that allows you to search for a Github user by username and display information from their profile. Uses React and Daisy-UI.</p>
         <p className='text-lg text-gray-400'>
-            Version <span className=''>1.0.0</span>
+            Version <span className=''>1.0.1</span>
         </p>
     </div>
   )
