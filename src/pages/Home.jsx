23cf24e4380fd5248224dda0a2423d347a225757
trav15 <travistokuyama@gmail.com>
2022-08-22 11:31:18 +0100

    Update title wording on Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import UserSearch from '../components/users/UserSearch';
 function Home() {
   return (
     <div>
-        <h1 className="text-6xl pb-6">Welcome To The Finder</h1>
+        <h1 className="text-6xl pb-6">Welcome To The Github Finder</h1>
         <UserSearch />
         <UserResults />
     </div>
